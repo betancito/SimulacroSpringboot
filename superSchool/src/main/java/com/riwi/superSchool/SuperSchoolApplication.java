884c078975badf058aef473901a1c24d2342eb1e
betancito <163953475+betancito@users.noreply.github.com>
2024-09-26 12:53:33 -0500

    Added necessary files
    Settled up basic project structure

---
+++ superSchool/src/main/java/com/riwi/superSchool/SuperSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.superSchool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperSchoolApplication.class, args);
+	}
+
+}
---
+++ superSchool/src/test/java/com/riwi/superSchool/SuperSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.superSchool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
