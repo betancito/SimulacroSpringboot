8393859a0cd0fcbf613eca5ae0e9099d27e84da6
betancito <jeronimobeta90@gmail.com>
2024-09-30 12:46:02 -0500

    Ongoing JWT service finished generators and extractors remaining to finilize checkers

---
+++ superSchool/src/main/java/com/riwi/superSchool/config/JwtAuthFilter.java
@@ -0,0 +1,34 @@
+package com.riwi.superSchool.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(
+            @NotNull HttpServletRequest request,
+            @NotNull HttpServletResponse response,
+            @NotNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String token;
+        final String userEmail;
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            token = authHeader.substring(7);
+            userEmail = jwtService.extractUsername(token);
+        }
+    }
+}
--- superSchool/src/main/java/com/riwi/superSchool/entities/UserEntity.java
+++ superSchool/src/main/java/com/riwi/superSchool/entities/UserEntity.java
@@ -3,6 +3,12 @@ package com.riwi.superSchool.entities;
 import com.riwi.superSchool.enums.UserRole;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @AllArgsConstructor
@@ -11,9 +17,9 @@ import lombok.*;
 @Setter
 @ToString
 @Table(name = "user")
-public class UserEntity{
+public class UserEntity implements UserDetails {
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy=GenerationType.SEQUENCE)
     private Long id;
 
     @Column
@@ -28,4 +34,34 @@ public class UserEntity{
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private UserRole role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
